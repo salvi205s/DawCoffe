4aa42a40048a6c2b1f1e3a40a739940d0aec5789
salvi205 <salvi205s@gmail.com>
2019-12-18 17:31:53 +0100

    Get y set

--- src/dawcoffe/Deposito.java
+++ src/dawcoffe/Deposito.java
@@ -16,4 +16,49 @@ public class Deposito {
             cantidadActual;
     private String contenido;
     private boolean reserva;
+
+    public double getCantidadMax() {
+        return cantidadMax;
+    }
+
+    public void setCantidadMax(double cantidadMax) {
+        this.cantidadMax = cantidadMax;
+    }
+
+    public double getCantidadUmbral() {
+        return cantidadUmbral;
+    }
+
+    public void setCantidadUmbral(double cantidadUmbral) {
+        this.cantidadUmbral = cantidadUmbral;
+    }
+
+    public double getCantidadActual() {
+        return cantidadActual;
+    }
+
+    public void setCantidadActual(double cantidadActual) {
+        this.cantidadActual = cantidadActual;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public boolean isReserva() {
+        return reserva;
+    }
+
+    public void setReserva(boolean reserva) {
+        this.reserva = reserva;
+    }
+    
+    
+    
+    
+    
 }
