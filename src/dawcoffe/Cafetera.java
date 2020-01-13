92fcf05e16baaed9cc3643d761ba6ca38254a994
salva <salvi205@hotmail.com>
2020-01-13 13:22:54 +0100

    crear Constructores

--- src/dawcoffe/Cafetera.java
+++ src/dawcoffe/Cafetera.java
@@ -15,22 +15,31 @@ import java.util.Scanner;
  */
 public class Cafetera {
 
-    //objeto scanner para introducir texto por teclado
-    private Scanner entrada = new Scanner(System.in);
-
-    //variables que iremos usando a lo largo del programa
+   //variables que iremos usando a lo largo del programa
     private double saldoAcumulado = 0,
             saldoCliente = 0,
             precio;
 
     private int numVentas = 0;
+    private Deposito cafe;
+    private Deposito cafeDescafeinado;
+    private Deposito azucar;
+    private Deposito chocolate;
+    private Deposito agua;
+    private Deposito leche;
 
-    private Deposito cafe = new Deposito(1000, 250, 1000, "cafe");
-    private Deposito cafeDescafeinado = new Deposito(1000, 250, 1000, "cafeDescafeinado");
-    private Deposito azucar = new Deposito(1000, 250, 1000, "azucar");
-    private Deposito chocolate = new Deposito(1000, 250, 1000, "chocolate");
-    private Deposito agua = new Deposito(1000, 250, 1000, "agua");
-    private Deposito leche = new Deposito(1000, 250, 1000, "leche");
+
+    public Cafetera(Deposito cafe, Deposito cafeDescafeinado, Deposito azucar, Deposito chocolate, Deposito agua, Deposito leche) {
+        this.cafe = cafe;
+        this.cafeDescafeinado = cafeDescafeinado;
+        this.azucar = azucar;
+        this.chocolate = chocolate;
+        this.agua = agua;
+        this.leche = leche;
+    }
+
+    public Cafetera() {
+    }
 
     //metodo para comprobar el precio del articulo
     public void comprobarPrecio() {
@@ -316,110 +325,3 @@ public class Cafetera {
     }
 
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package dawcoffe;
-
-import java.awt.Menu;
-import java.util.Scanner;
-
-/**
- *
- * @author Salva
- */
-public class Cafetera {
-
-    //objeto scanner para introducir texto por teclado
-    private Scanner entrada = new Scanner(System.in);
-
-    //variables que iremos usando a lo largo del programa
-    private double saldoAcumulado = 0,
-            saldoCliente = 0,
-            precio;
-
-    private int numVentas=0;
-
-//        Deposito cafe = new Deposito(1000, 250, 1000, "cafe");
-//        Deposito cafeDescafeinado = new Deposito(1000, 250, 1000, "cafeDescafeinado");
-//        Deposito azucar = new Deposito(1000, 250, 1000, "azucar");
-//        Deposito chocolate = new Deposito(1000, 250, 1000, "chocolate");
-//        Deposito agua = new Deposito(1000, 250, 1000, "agua");
-//        Deposito leche = new Deposito(1000, 250, 1000, "leche");
-    //metodo para comprobar el precio del articulo
-    public void comprobarPrecio() {
-
-        //si no ha introducido el precio minimo para el articulo, avisara del saldo insuficiente
-        while (getSaldoCliente() <= precio) {
-
-            System.out.println("Saldo insuficiente " + this.saldoCliente);
-            //introduce el dinero
-            System.out.println("Introduzca mas monedas");
-            System.out.println("saldoCliente " + getSaldoCliente());
-
-            saldoCliente += entrada.nextDouble();
-            setSaldoCliente(saldoCliente);
-            System.out.println("saldoCliente " + getSaldoCliente());
-
-        };
-
-    }
-
-    //muestra informacion del producto elegido
-    public void mostrarInformacion(String opcion, String opcionDescafeinado, String cantAzucar) {
-        double cambio = 0;
-
-        //si el precio es mayor o igual al del articulo muestra la informacion
-        if (getSaldoCliente() >= getPrecio()) {
-            System.out.println("Ha elegido " + opcion);
-            System.out.println("Descafeinado: " + opcionDescafeinado);
-            System.out.println("Con " + cantAzucar + " azucar");
-
-            //calcula el cambio a devolver
-            cambio = getSaldoCliente() - this.precio;
-            System.out.printf("Su cambio es: %.2f %n", cambio);
-            //suma el saldo al saldoAcumulado
-            setSaldoAcumulado(this.precio);
-            //incrementa en 1 el numero de ventas
-            setNumVentas(1);
-        }
-    }
-
-    //getters y setters
-    public void setSaldoAcumulado(double saldoAcumulado) {
-        this.saldoAcumulado += saldoAcumulado;
-    }
-
-    public void setSaldoCliente(double saldoCliente) {
-        this.saldoCliente = saldoCliente;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-
-    public void setNumVentas(int numVentas) {
-        this.numVentas += numVentas;
-    }
-
-    public double getSaldoAcumulado() {
-        return saldoAcumulado;
-    }
-
-    public double getSaldoCliente() {
-        return saldoCliente;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public int getNumVentas() {
-        return numVentas;
-    }
-
-}
-
