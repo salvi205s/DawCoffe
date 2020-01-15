84c1bd3bbd498c11cb5a878226280d36fca83973
salvi205 <salvi205s@gmail.com>
2020-01-15 10:40:31 +0100

    Crear depositos, cafetera que recibe los depositos y menuInterfaz que recibe la cafetera

--- src/dawcoffe/Programa.java
+++ src/dawcoffe/Programa.java
@@ -13,15 +13,15 @@ public class Programa {
 
     public static void main(String[] args) {
 
-//        Deposito cafe = new Deposito(1000, 250, 1000, "cafe");
-//        Deposito cafeDescafeinado = new Deposito(1000, 250, 1000, "cafeDescafeinado");
-//        Deposito azucar = new Deposito(1000, 250, 1000, "azucar");
-//        Deposito chocolate = new Deposito(1000, 250, 1000, "chocolate");
-//        Deposito agua = new Deposito(1000, 250, 1000, "agua");
-//        Deposito leche = new Deposito(1000, 250, 1000, "leche");
-////        Se crea el objeto menu
-//        Cafetera cafetera = new Cafetera(cafe, cafeDescafeinado, azucar, chocolate, agua, leche);
-        MenuInterfaz menu = new MenuInterfaz();
+        Deposito cafe = new Deposito(1000, 250, 3, "cafe");
+        Deposito cafeDescafeinado = new Deposito(1000, 250, 1000, "cafeDescafeinado");
+        Deposito azucar = new Deposito(1000, 250, 1000, "azucar");
+        Deposito chocolate = new Deposito(1000, 250, 1000, "chocolate");
+        Deposito agua = new Deposito(1000, 250, 1000, "agua");
+        Deposito leche = new Deposito(1000, 250, 1000, "leche");
+//        Se crea el objeto menu
+        Cafetera cafetera = new Cafetera(cafe, cafeDescafeinado, azucar, chocolate, agua, leche);
+        MenuInterfaz menu = new MenuInterfaz(cafetera);
 
         //y se llama
         menu.menu();
