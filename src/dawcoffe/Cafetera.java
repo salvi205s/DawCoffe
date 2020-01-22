0248956a3845c6bbf618201f603cae236e1d5142
salva <salvi205@hotmail.com>
2020-01-22 14:35:25 +0100

    Clase comentada

--- src/dawcoffe/Cafetera.java
+++ src/dawcoffe/Cafetera.java
@@ -14,15 +14,14 @@ import java.util.Scanner;
  */
 public class Cafetera {
 
-    //objeto scanner para introducir texto por teclado
-//    private Scanner entrada = new Scanner(System.in);
-    //variables que iremos usando a lo largo del programa
+    //variables que necesitaremos a lo largo del programa
     private double saldoAcumulado = 0,
             saldoCliente = 0,
             precio;
 
     private int numVentas = 0;
 
+    //creamos los depositos que le pasamos a la cafetera
     private Deposito cafe;
     private Deposito cafeDescafeinado;
     private Deposito azucar;
@@ -31,6 +30,7 @@ public class Cafetera {
     private Deposito leche;
     private Deposito sacarina;
 
+    //constructor parametrizado, que recibe los depositos
     public Cafetera(Deposito cafe, Deposito cafeDescafeinado, Deposito azucar, Deposito chocolate, Deposito agua, Deposito leche, Deposito sacarina) {
         this.cafe = cafe;
         this.cafeDescafeinado = cafeDescafeinado;
@@ -41,31 +41,36 @@ public class Cafetera {
         this.sacarina = sacarina;
     }
 
+//    constructor por defecto
     public Cafetera() {
     }
 
-//    public void servirCafe(String menu, int codigo) {
-//
-//        restarCafe(codigo);
-//
-//    }
-    public void servirChocolate(String menu, int codigo) {
+    //metodo que englova los metodos necesarios para servir un chocolate
+    public void servirChocolate() {
 
-//resta la cantidad de chocolate al deposito
+        //resta la cantidad de chocolate, y de leche, al deposito
         restarChocolate(12);
-        restarLeche(180);
+        servirLeche();
+
+    }
+    //metodo que englova los metodos necesarios para servir un chocolate
+
+    public void servirLeche() {
+
+        //resta la cantidad de agua y de leche, al deposito
+        restarLeche(30);
+        agua.servirContenido(180);
 
     }
 
-//    public void servirLeche(String menu, int codigo) {
-//
-//        restarLeche(180);
-//    }
     //metodo para comprobar el precio del articulo
     public boolean comprobarPrecio() {
 
+        //variable booleana, iniciada a false
         boolean precioCorrecto = false;
 
+        //si el dinero ingresado por el cliente, es mayor que el precio del articulo
+        //pone precio correcto a true
         if (getSaldoCliente() >= getPrecio()) {
             precioCorrecto = true;
         }
@@ -73,37 +78,50 @@ public class Cafetera {
         return precioCorrecto;
     }
 
+    //metodo para devolver el cambio
     public double devolverCambio() {
+
+        //inicio la variable cambio a cero
         double cambio = 0;
 
         //calcula el cambio a devolver
         cambio = getSaldoCliente() - this.precio;
 
-//        //suma el saldo al saldoAcumulado
+        //suma el precio al saldoAcumulado
         setSaldoAcumulado(this.precio);
+
         //incrementa en 1 el numero de ventas
         setNumVentas(1);
 
         return cambio;
     }
 
+    //metodo para informar si hay existencias
     public boolean informarFaltaExistencias() {
+
+        //variable booleana, iniciada a false
         boolean faltaExistencias = false;
 
+        //si la cantidad actual de los depositos es menor que la cantidad minima a servir,
+        //la variable faltaExistencias, se pone a true(Que faltan existencias)
         if (agua.getCantidadActual() < 50 || leche.getCantidadActual() < 180 || cafe.getCantidadActual() < 8 || chocolate.getCantidadActual() < 12) {
             faltaExistencias = true;
         }
         return faltaExistencias;
     }
 
-    //metodos para restar el contenido de los depositos
+    //metodos para restar el contenido del deposito de leche
     public void restarLeche(int cantidad) {
+
+        //llama al metodo servirContenido, de la clase deposito
         leche.servirContenido(cantidad);
 
     }
 
-    //metodo deposito de cafe
+    //metodo que resta el contenido del deposito de cafe
     public void restarCafe(int codigo) {
+
+        //dependiendo del codigo(eleccion) resta la cantidad delos depositos, relacionados con cafe
         switch (codigo) {
             case 101:
                 cafe.servirContenido(8);
@@ -151,14 +169,15 @@ public class Cafetera {
 
     }
 
+    //metodo que resta chocolate, del deposito
     public void restarChocolate(int cantidad) {
         chocolate.servirContenido(cantidad);
 
     }
 
+    //metodo que resta azucar del deposito, dependiendo de la cantidad que elija el usuario
     public void restarAzucar(int cantidadAzucar) {
 
-        System.out.println("Ha elegido " + cantidadAzucar);
         //resta el contenido de azucar al deposito
         switch (cantidadAzucar) {
             case 1:
@@ -186,11 +205,12 @@ public class Cafetera {
 
     }
 
+    //resta el contenido de sacarina al deposito
     public void restarSacarina() {
         sacarina.servirContenido(6);
     }
 
-//-------------------------------------------------------------------------------------------------------------------------------------
+    //metodo para rellenar el deposito parcialmente, recibe una eleccion de deposito y una cantidad
     public void rellenarDepositoParcialmente(int opcionDeposito, double cantidad) {
 
         switch (opcionDeposito) {
@@ -225,8 +245,8 @@ public class Cafetera {
                 break;
         }
 
+        //se controla que la cantidad que se le pasa, no sea mayor que la capacidad maxima
         if (cantidad > cafe.getCantidadMax()) {
-
             cafe.setCantidadActual(cafe.getCantidadMax());
         }
         if (cantidad > cafeDescafeinado.getCantidadMax()) {
@@ -250,6 +270,7 @@ public class Cafetera {
 
     }
 
+    //metodo para rellenar el deposito completo, recibe la opcion del deposito
     public void rellenarDepositoCompleto(int opcionDeposito) {
         switch (opcionDeposito) {
             case 1:
@@ -275,13 +296,13 @@ public class Cafetera {
             case 6:
                 leche.llenarDepositoCompleto();
                 break;
-             case 7:
+            case 7:
                 sacarina.llenarDepositoCompleto();
                 break;
             case 8:
 
                 break;
-        
+
         }
 
     }
