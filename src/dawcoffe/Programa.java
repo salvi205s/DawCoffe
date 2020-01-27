439295229dcf182cbf13887583b15e792b433a35
salvi205 <salvi205s@gmail.com>
2020-01-27 11:51:31 +0100

    Comentar la clase programa

--- src/dawcoffe/Programa.java
+++ src/dawcoffe/Programa.java
@@ -13,6 +13,7 @@ public class Programa {
 
     public static void main(String[] args) {
 
+        //creamos los depositos para pasarselos a la cafetera
         Deposito cafe = new Deposito(1000, 250, 1000, "cafe");
         Deposito cafeDescafeinado = new Deposito(1000, 250, 1000, "cafeDescafeinado");
         Deposito azucar = new Deposito(1000, 250, 1000, "azucar");
@@ -20,11 +21,13 @@ public class Programa {
         Deposito agua = new Deposito(1000, 250, 1000, "agua");
         Deposito leche = new Deposito(1000, 250, 1000, "leche");
         Deposito sacarina = new Deposito(1000, 250, 1000, "sacarina");
-//        Se crea el objeto menu
+
+        //creamos la cafetera que recibe los depositos
         Cafetera cafetera = new Cafetera(cafe, cafeDescafeinado, azucar, chocolate, agua, leche, sacarina);
+        //creamos el objeto menu que recibe una cafetera
         MenuInterfaz menu = new MenuInterfaz(cafetera);
 
-        //y se llama
+        //y se llama, al metodo menu de la clase menu
         menu.menu();
 
     }
